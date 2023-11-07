e377b967854a9e0c2e00cce739df2dbfe339ef11
chessNori <yhs8864@gmail.com>
2023-11-07 19:25:42 +0900

    second commit

--- yhs_module.py
+++ yhs_module.py
@@ -3,7 +3,33 @@ import librosa
 import os
 from glob import glob
 import soundfile as sf
-import tensorflow as tf
+# import tensorflow as tf
+import torch
+
+
+def make_mask(wave_mag, th_hi=-0.9, th_lo=-0.7, time_cut=15):
+    mask = np.zeros_like(wave_mag)
+    for i in range(wave_mag.shape[0]):
+        for j in range(wave_mag.shape[1]):
+            if (wave_mag[i][j] > th_hi) and (i > 128):
+                mask[i][j] = 1
+            elif (wave_mag[i][j] > th_lo) and (i <= 128):
+                mask[i][j] = 1
+
+    time_masking = np.sum(mask, axis=0)
+
+    for i in range(time_masking.shape[0]):
+        if time_masking[i] < time_cut:
+            mask[:, i] -= mask[:, i]  # time masking
+            time_masking[i] = 0
+        else:
+            time_masking[i] = 1
+
+    mask[0:4, :] = 0  # remove DC
+
+    mask = np.transpose(mask, (1, 0))
+
+    return time_masking, mask
 
 
 def adjust_snr(target, noise, db):  # Because of abs, it didn't return good scale value. We need bug fix
@@ -68,8 +94,8 @@ def snr(original, denoise):
     sum_noise = original - denoise
     sum_original = np.power(np.abs(original), 2)
     sum_noise = np.power(np.abs(sum_noise), 2)
-    sum_original = np.sum(sum_original)
-    sum_noise = np.sum(sum_noise)
+    sum_original = np.sum(sum_original, axis=-1)
+    sum_noise = np.sum(sum_noise, axis=-1)
     res = np.log10(sum_original/sum_noise) * 10
 
     return res
@@ -178,41 +204,45 @@ class SpectralDomainData(TimeDomainData):
         super(SpectralDomainData, self).__init__(win_size)
         self.dc = dc  # Retain DC bin
         self.y_form = y_form
-        # self.n_fft = win_size
 
     def np_spec(self, data):
-        data = librosa.stft(data, n_fft=self.frame_size, hop_length=self.frame_size // 2,
+        spec_data = np.concatenate((np.zeros(data.shape[0], self.frame_size//2), data), axis=-1)  # padding
+        spec_data = librosa.stft(spec_data, n_fft=self.frame_size, hop_length=self.frame_size // 2,
                             win_length=self.frame_size, window='hann', center=False)
-        data = np.transpose(data, (0, 2, 1))
+        spec_data = np.transpose(spec_data, (0, 2, 1))
         if self.dc is False:
-            data = cut_dc(data)
+            spec_data = cut_dc(spec_data)
 
-        return data
+        return spec_data
 
-    def tf_spec(self, data):
-        data = tf.signal.stft(data, self.frame_size, self.frame_size // 2, self.frame_size)
+    # def tf_spec(self, data):
+    #     spec_data = tf.signal.stft(data, self.frame_size, self.frame_size // 2, self.frame_size)
+    #     if self.dc is False:
+    #         spec_data = cut_dc(spec_data)
+    #
+    #     return spec_data
+
+    def torch_spec(self, data):
+        sine_window = (torch.Tensor(range(self.frame_size + 2))
+                       * (4.0 * torch.atan(torch.Tensor([1.0]))) / (self.frame_size + 2))
+        sine_window = torch.sin(sine_window[1:-1])
+        spec_data = np.concatenate((data, np.zeros(data.shape[0], self.frame_size // 2)), axis=-1)  # padding
+        spec_data = torch.Tensor(spec_data)
+        spec_data = torch.stft(spec_data, window=sine_window, n_fft=self.frame_size, hop_length=self.frame_size // 2,
+                                 win_length=self.frame_size, center=False, return_complex=False)
+        spec_data = torch.transpose(spec_data, 1, 2)
         if self.dc is False:
-            data = cut_dc(data)
+            spec_data[:, :, :, 0] = cut_dc(spec_data[:, :, :, 0])
+            spec_data[:, :, :, 1] = cut_dc(spec_data[:, :, :, 1])
 
-        return data
+        return spec_data
 
     def dataset(self, x_y_snr, form='numpy'):  # @overrides
         x_data, y_data = self.pre_function(x_y_snr)
         y_data_real = None
         y_data_imag = None
 
-        if form == 'tensorflow':
-            x_data = self.tf_spec(x_data)
-            x_data_real = tf.math.real(x_data)
-            x_data_imag = tf.math.imag(x_data)
-
-            if self.y_form != 'time':
-                y_data = self.tf_spec(y_data)
-
-                y_data_real = tf.math.real(y_data)
-                y_data_imag = tf.math.imag(y_data)
-
-        elif form == 'numpy':
+        if form == 'numpy':
             x_data = self.np_spec(x_data)
             x_data_real = x_data.real.astype(np.float32)
             x_data_imag = x_data.imag.astype(np.float32)
@@ -221,6 +251,22 @@ class SpectralDomainData(TimeDomainData):
                 y_data = self.np_spec(y_data)
                 y_data_real = y_data.real.astype(np.float32)
                 y_data_imag = y_data.imag.astype(np.float32)
+
+        # elif form == 'tensorflow':
+        #     x_data = self.tf_spec(x_data)
+        #     x_data_real = tf.math.real(x_data)
+        #     x_data_imag = tf.math.imag(x_data)
+        #
+        #     if self.y_form != 'time':
+        #         y_data = self.tf_spec(y_data)
+        #
+        #         y_data_real = tf.math.real(y_data)
+        #         y_data_imag = tf.math.imag(y_data)
+
+        elif form == 'torch':
+            x_data = self.torch_spec(x_data)
+            x_data_real = x_data[:, :, :, 0]
+            x_data_imag = x_data[:, :, :, 0]
 
         else:
             print("Method for this dataset is not ready")
@@ -247,13 +293,13 @@ class SpectralMagnitudeData(SpectralDomainData):
         x_data = self.np_spec(x_data)
         x_data, x_phase = librosa.magphase(x_data)
         if form == 'log':
-            x_data = np.log10(x_data)  # ******************************************log 0?
+            x_data = np.log10(x_data + 1.0e-7)
 
         if self.y_form != 'time':
             y_data = self.np_spec(y_data)
             y_data, y_phase = librosa.magphase(y_data)
             if form == 'log':
-                y_data = np.log10(y_data)  # ******************************************log 0?
+                y_data = np.log10(y_data + 1.0e-7)
 
         x_data.astype(np.float32)
         y_data.astype(np.float32)
